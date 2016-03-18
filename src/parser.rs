19e8b2475630dbcc625bde3ab34cb070ce9991c0
holyshared <holy.shared.design@gmail.com>
2016-03-18 13:52:50 +0900

    add hit counter

--- src/parser.rs
+++ src/parser.rs
@@ -4,13 +4,15 @@ use report:: { Report, FileResult };
 use coverage:: { Coverage };
 
 pub struct ReportParser<R> {
-    parser: LCOVParser<R>
+    parser: LCOVParser<R>,
+    files: Vec<FileResult>,
+    counter: Option<HitCounter>
 }
 
 impl<R: Read> ReportParser<R> {
     pub fn new(reader: R) -> Self {
         let parser = LCOVParser::new(reader);
-        ReportParser { parser: parser }
+        ReportParser { parser: parser, files: vec!(), counter: None }
     }
     pub fn parse(&mut self) -> Result<Report> {
         loop {
@@ -29,7 +31,7 @@ impl<R: Read> ReportParser<R> {
         Ok(report)
     }
 
-    pub fn on_parsed(&self, record: LCOVRecord) {
+    pub fn on_parsed(&mut self, record: LCOVRecord) {
         match record {
             LCOVRecord::SourceFile(name) => self.on_source_file(name),
             LCOVRecord::Data(_, excution_count, _) => self.on_data(excution_count),
@@ -38,16 +40,51 @@ impl<R: Read> ReportParser<R> {
         }
     }
 
-    pub fn on_source_file(&self, name: String) {
+    pub fn on_source_file(&mut self, name: String) {
         println!("{}", name);
+        self.counter = Some(HitCounter::new(name));
     }
 
-    pub fn on_data(&self, excution_count: u32) {
+    pub fn on_data(&mut self, excution_count: u32) {
         println!("{}", excution_count);
+        match self.counter.as_mut() {
+            Some(counter) => counter.proceed(excution_count),
+            None => {}
+        }
     }
 
-    pub fn on_end_of_record(&self) {
+    pub fn on_end_of_record(&mut self) {
         println!("{}", "excution_count");
+        match self.counter.as_mut() {
+            Some(counter) => self.files.push(FileResult::new(counter.name(), Coverage::new(0.1))),
+            None => {}
+        }
+    }
+}
+
+pub struct HitCounter {
+    name: String,
+    found: u32,
+    hit: u32
+}
+
+impl HitCounter {
+    pub fn new(name: String) -> Self {
+        HitCounter {
+            name: name,
+            found: 0,
+            hit: 0
+        }
+    }
+    pub fn name(&self) -> String {
+        self.name.clone()
+    }
+    pub fn proceed(&mut self, excution_count: u32) {
+        self.found += 1;
+        if excution_count <= 0 {
+            return;
+        }
+        self.hit += 1;
     }
 }
 
