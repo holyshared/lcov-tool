e3662cf4684437877a4f4a8717b7b61714967454
holyshared <holy.shared.design@gmail.com>
2016-05-03 16:59:10 +0900

    Upgrade lcov-parser

--- src/parser.rs
+++ src/parser.rs
@@ -1,41 +1,35 @@
-use std::io:: { Read, Result };
-use lcov_parser:: { LCOVParser, LCOVRecord, ParsedResult };
+use std::result:: { Result };
+use lcov_parser:: { LCOVParser, LCOVRecord, RecordParseError };
 use report:: { Report, FileResult };
 use coverage:: { Coverage };
 
-pub struct ReportParser<R> {
-    parser: LCOVParser<R>,
+pub struct ReportParser {
+    parser: LCOVParser,
     files: Vec<FileResult>,
     counter: Option<HitCounter>
 }
 
-impl<R: Read> ReportParser<R> {
-    pub fn new(reader: R) -> Self {
-        let parser = LCOVParser::new(reader);
+impl ReportParser {
+    pub fn new(report: &str) -> Self {
+        let parser = LCOVParser::new(report);
         ReportParser { parser: parser, files: vec!(), counter: None }
     }
-    pub fn parse(&mut self) -> Result<Report> {
-        loop {
-            match self.parser.parse_next() {
-                ParsedResult::Eof => { break; },
-                ParsedResult::Ok(record, _) => self.on_parsed(record),
-                ParsedResult::Err(error) => panic!("{:?}", error)
-            }
+    pub fn parse(&mut self) -> Result<Report, RecordParseError> {
+        let records = try!(self.parser.parse());
+
+        for record in records.iter() {
+            self.on_parsed(record);
         }
 
-        let report = Report::new(vec!(
-            FileResult::new("test1.rs", Coverage::new(0.1)),
-            FileResult::new("test2.rs", Coverage::new(0.2)),
-            FileResult::new("test3.rs", Coverage::new(0.3))
-        ));
+        let report = Report::new(self.files.clone());
         Ok(report)
     }
 
-    pub fn on_parsed(&mut self, record: LCOVRecord) {
+    pub fn on_parsed(&mut self, record: &LCOVRecord) {
         match record {
-            LCOVRecord::SourceFile(name) => self.on_source_file(name),
-            LCOVRecord::Data(_, excution_count, _) => self.on_data(excution_count),
-            LCOVRecord::EndOfRecord => self.on_end_of_record(),
+            &LCOVRecord::SourceFile(ref name) => self.on_source_file(name.clone()),
+            &LCOVRecord::Data(_, excution_count, _) => self.on_data(excution_count),
+            &LCOVRecord::EndOfRecord => self.on_end_of_record(),
             _ => {}
         }
     }
@@ -92,11 +86,15 @@ impl HitCounter {
 mod tests {
     use std::fs:: { File };
     use parser:: { ReportParser };
+    use std::io:: { Read };
 
     #[test]
     fn test_parse_report() {
-        let f = File::open("tests/fixtures/report.lcov").unwrap();
-        let mut parser = ReportParser::new(f);
+        let mut buffer = String::new();
+        let mut f = File::open("tests/fixtures/report.lcov").unwrap();
+        let _ = f.read_to_string(&mut buffer);
+
+        let mut parser = ReportParser::new(buffer.as_str());
         let report = parser.parse().unwrap();
         assert_eq!(report.files().len(), 2);
     }
