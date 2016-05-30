cd30fc27ab0bb9427f09a1550de9a987894ef00c
holyshared <holy.shared.design@gmail.com>
2016-05-30 10:07:19 +0900

    add cli interface

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,28 @@ mod report;
 mod parser;
 mod processor;
 
+extern crate clap;
 extern crate lcov_parser;
 
+use clap:: { App, SubCommand, Arg };
+
 fn main() {
+    let file = Arg::with_name("file")
+        .value_name("FILE")
+        .help("The report file of LCOV");
+    let coverage = SubCommand::with_name("coverage")
+        .arg(file);
+
+    let app = App::new("lcovtool")
+        .version("1.0")
+        .author("Noritaka Horio <holy.shared.design@gmail.com>")
+        .about("LCOV report of utility tool")
+        .subcommand(coverage);
+
+    let matches = app.get_matches();
+
+    match matches.subcommand_name() {
+        Some("coverage") => { println!("{:?}", matches); },
+        _ => { }
+    }
 }
